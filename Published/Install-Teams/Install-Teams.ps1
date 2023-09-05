922f71010b7a38028d889b992aff5e25704ea687
Chris Calverley <calverley79@yahoo.com>
2023-09-05 14:37:30 -0500

    make msiexec wait before verification

--- Published/Install-Teams/Install-Teams.ps1
+++ Published/Install-Teams/Install-Teams.ps1
@@ -51,7 +51,7 @@ Write-Log -Message 'Downloading Teams MWI' -Type LOG
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -UseBasicParsing -Uri $Installer -OutFile "$DownloadLocation\$FileName"
 
-& msiexec.exe /I "C:\Temp\$BaseName\$FileName" /qn /Norestart ALLUSERS=1
+Start-Process (msiexec.exe /I "C:\Temp\$BaseName\$FileName" /qn /Norestart ALLUSERS=1) -Wait
 
 #Verify
 if (!(Test-Path "C:\Program Files\Teams Installer") -and (!(Test-Path "C:\\Program Files (x86)\Teams Installer"))) {
