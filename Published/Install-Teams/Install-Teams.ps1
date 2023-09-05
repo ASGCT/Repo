7ff389193c300dfbdc0196b94dd7eca94be1b53b
Chris Calverley <calverley79@yahoo.com>
2023-09-05 09:10:50 -0500

    move usebasicparsing

--- Published/Install-Teams/Install-Teams.ps1
+++ Published/Install-Teams/Install-Teams.ps1
@@ -23,7 +23,7 @@ Param()
 
 If (!($bootstraploaded)){
     Set-ExecutionPolicy Bypass -scope Process -Force
-    $BaseRepoUrl = (Invoke-webrequest -URI -UseBasicParsing "https://raw.githubusercontent.com/ASGCT/Repo/main/Environment/Bootstrap.ps1").Content
+    $BaseRepoUrl = (Invoke-webrequest -UseBasicParsing -URI "https://raw.githubusercontent.com/ASGCT/Repo/main/Environment/Bootstrap.ps1").Content
     $scriptblock = [scriptblock]::Create($BaseRepoUrl)
     Invoke-Command -ScriptBlock $scriptblock
 
