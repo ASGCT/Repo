1e985a7021b2e0de76fd278bf78ce0267f132a04
Chris Calverley <calverley79@yahoo.com>
2023-09-19 09:50:45 -0500

    winget move

--- Published/Invoke-Winget/Invoke-Winget.ps1
+++ Published/Invoke-Winget/Invoke-Winget.ps1
@@ -55,38 +55,42 @@ Install-Module -Name NuGet -Force -WarningAction SilentlyContinue -ErrorAction S
 Install-Module -Name Microsoft.WinGet.Client -Force -WarningAction SilentlyContinue -ErrorAction SilentlyContinue 
 Import-module microsoft.winget.client -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
 #winget list --accept-source-agreements
-Start-Process powershell -verb RunAs -argumentlist "winget list --accept-source-agreements"
-Switch ($Action) {
-  'Install' {
-      $arugment = 'Install '
-  }
-  'Upgrade' {
-      $arugment = 'Upgrade '
-  }
-  'Uninstall' {
-      $arugment = 'Uninstall '
-  }
-  default {
-    Write-log -message 'ERROR : An Unhandled exception has occurred' -type ERROR
-    throw 'ERROR : An Unhandled exception has occurred'
-  }
-}
 
-If ($type -eq 'ID') {
-  $argument = "$argument--ID "
-}
+#WebClient
+$dc = New-Object net.webclient
+$dc.UseDefaultCredentials = $true
+$dc.Headers.Add("user-agent", "Inter Explorer")
+$dc.Headers.Add("X-FORMS_BASED_AUTH_ACCEPTED", "f")
 
-<# Available CMDlets
-Get-WinGetVersion 
-Find-WinGetPackage 
-Get-WinGetPackage 
-Get-WinGetSource 
-Install-WinGetPackage 
-Uninstall-WinGetPackage 
-Update-WinGetPackage 
-Get-WinGetUserSettings 
-Set-WinGetUserSettings 
-Test-WinGetUserSettings 
-Assert-WinGetPackageManager 
-Repair-WinGetPackageManager
-#>
+#temp folder
+$InstallerFolder = $(Join-Path $env:ProgramData CustomScripts)
+if (!(Test-Path $InstallerFolder))
+{
+New-Item -Path $InstallerFolder -ItemType Directory -Force -Confirm:$false
+}
+	#Check Winget Install
+	Write-Host "Checking if Winget is installed" -ForegroundColor Yellow
+	$TestWinget = Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq "Microsoft.DesktopAppInstaller"}
+	If ([Version]$TestWinGet. Version -gt "2022.506.16.0") 
+	{
+		Write-Host "WinGet is Installed" -ForegroundColor Green
+	}Else 
+		{
+		#Download WinGet MSIXBundle
+		Write-Host "Not installed. Downloading WinGet..." 
+		$WinGetURL = "https://aka.ms/getwinget"
+		$dc.DownloadFile($WinGetURL, "$InstallerFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
+		
+		#Install WinGet MSIXBundle 
+		Try 	{
+			Write-Host "Installing MSIXBundle for App Installer..." 
+			Add-AppxProvisionedPackage -Online -PackagePath "$InstallerFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -SkipLicense 
+			Write-Host "Installed MSIXBundle for App Installer" -ForegroundColor Green
+			}
+		Catch {
+			Write-Host "Failed to install MSIXBundle for App Installer..." -ForegroundColor Red
+			} 
+	
+		#Remove WinGet MSIXBundle 
+		#Remove-Item -Path "$InstallerFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Force -ErrorAction Continue
+		}
