052bb870e6c0e0a5b4127d0751081c9d8fa0c93f
Chris Calverley <calverley79@yahoo.com>
2023-10-04 13:49:43 -0500

    complete script for testing

--- Published/Set-FirefoxExtension/Set-FirefoxExtension.ps1
+++ Published/Set-FirefoxExtension/Set-FirefoxExtension.ps1
@@ -1,39 +1,41 @@
 <#
   .SYNOPSIS
-  Uninstalls All ConnectWise Control instances
+  Install extensions on firefox browsers
 
   .DESCRIPTION
-  The Uninstall-ScreenConnect.ps1 script removes ConnectWise Control instances from target machine.
+  Adds an extension to the Firefox browser
   
-  .PARAMETER organizationKey
-  Specifies the organization key assigned by skykick when you activate a migration job.
+  .PARAMETER ExtensionUrl
+  right-click the download button in the app store and select copy link address
 
   .INPUTS
-  InstanceID (Which can be found in the software list contained in the ()'s for the instance)  
+  ExtensionUrl
 
   .OUTPUTS
   System.String
-  C:\Temp\Uninstall-Screenconnect.log  
+  C:\Temp\Set-FirefoxExtension.log
 
   .EXAMPLE
-  PS> .\Uninstall-Screenconnect.ps1 
-  Removes all installed instances of Screenconnect Client from target machine.
-
-  .EXAMPLE
-  PS> .\Uninstall-Screenconnect.ps1 -InstanceID g4539gjdsfoir
-  Only removes ScreenConnect Client (g4539gjdsfoir) from the target machine.
+  PS> .\Set-FirefoxExtension.ps1 -ExtensionUrl https://addons.mozilla.org/firefox/downloads/file/4168788/1password_x_password_manager-2.15.1.xpi
+  Installs 1Password on firefox browsers.
 
   .NOTES
   This script was developed by
   Chris Calverley 
   on
-  September 07, 2023
+  October 04, 2023
   For
   ASGCT
 #>
 
 [CmdletBinding()]
-Param()
+Param(
+  # Parameter help description
+  [Parameter(Mandatory=$true)]
+  [String[]]
+  $ExtensionUrl
+
+)
 
 If (!($bootstraploaded)){
     Set-ExecutionPolicy Bypass -scope Process -Force
@@ -42,4 +44,47 @@ If (!($bootstraploaded)){
     $scriptblock = [scriptblock]::Create($BaseRepoUrl)
     Invoke-Command -ScriptBlock $scriptblock
 
+}
+
+$extensionPath = 'C:\Firefox-Extensions'
+
+If (!(Test-path $extensionPath)){
+  New-Item -Name $extensionPath -ItemType Directory -force | Out-Null
+}
+
+Foreach ($url in $ExtensionUrl) {
+  $Uri -match '(?<=/)(?<ExtensionName>[^/]+)(?=\?)'
+  $Extension = $matches['ExtensionName']
+
+  Invoke-WebRequest -Uri $url -OutFile "$extensionPath\$Extension"
+}
+
+Get-ChildItem -Path $ExtensionPath | Foreach-Object { $NewName = $_.FullName -replace ".xpi", ".zip" 
+Copy-Item -Path $_.FullName -Destination $NewName }
+
+Expand-Archive -Path (Get-ChildItem $ExtensionPath |
+Where-Object { $_.Extension -eq '.zip'} | Select-Object -ExpandProperty FullName) -DestinationPath $ExtensionPath
+
+$jsonContent = Get-Content "$ExtensionPath\manifest.json" | ConvertFrom-Json
+$NewValues = $jsonContent.applications.gecko.id
+
+Rename-Item -Path $ExtensionPath\$($matches['ExtensionName']) -NewName "$NewValues.xpi"
+Remove-Item -Path $ExtensionPath -Exclude *.xpi -Recurse -Force
+
+If([environment]::Is64BitOperatingSystem) {
+  If (Test-Path "C:\Program Files\Mozilla Firefox\firefox.exe") {
+          
+    $regKey = "HKLM:\Software\Mozilla\Firefox\Extensions"
+    New-ItemProperty -Path $regKey -Name $authorValue -Value "$ExtensionPath\$authorValue.xpi" -PropertyType String
+  } Else {
+          
+          $regKey = "HKLM:\Software\Wow6432Node\Mozilla\Firefox\Extensions"
+          
+          New-ItemProperty -Path $regKey -Name $authorValue -Value "$ExtensionPath\$authorValue.xpi" -PropertyType String
+      }
+
+} else {
+  $regKey = "HKLM:\Software\Mozilla\Firefox\Extensions"
+      New-ItemProperty -Path $regKey -Name $matches['ExtensionName'] -Value "$ExtensionPath\$($matches['ExtensionName'])" -PropertyType String
+  
 }
