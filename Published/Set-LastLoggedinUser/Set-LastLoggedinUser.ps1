b2899c27d9139553380bb581cfd6a5d7d3c6cb93
Chris Calverley <calverley79@yahoo.com>
2023-11-07 10:56:21 -0600

    adjust

--- Published/Set-LastLoggedinUser/Set-LastLoggedinUser.ps1
+++ Published/Set-LastLoggedinUser/Set-LastLoggedinUser.ps1
@@ -70,7 +70,7 @@ Function Get-Sid {
 $regName = 'LastLoggedOnUser'
 $regPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI'
 $regSAMUser = 'LastLoggedOnSAMUser'
-$LLOUSID = 'LastLoggedOnUser'
+$LLOUSID = 'LastLoggedOnUserSID'
 $SUSID = 'SelectedUserSID'
 
 if (-not (Test-Path $regPath)) {
